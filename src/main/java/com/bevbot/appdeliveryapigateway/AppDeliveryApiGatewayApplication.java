517cd9fbb7c8c7311eb1e2a76e3293974ee36a31
Sam Allen <Sam.Allen@beveragebot.com>
2020-01-10 23:40:47 -0800

    first commit

---
+++ src/main/java/com/bevbot/appdeliveryapigateway/AppDeliveryApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bevbot.appdeliveryapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeliveryApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDeliveryApiGatewayApplication.class, args);
+    }
+
+}
